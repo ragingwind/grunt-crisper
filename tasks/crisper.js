d90c69c09d7282646b3dc95744d115abac14ca91
ragingwind <ragingwind@gmail.com>
2015-04-20 21:31:52 +0900

    init

---
+++ tasks/crisper.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var path = require('path');
+var crisper = require('crisper');
+
+function getDestPath(filepath) {
+	var basename = path.basename(filepath, path.extname(filepath));
+	var base = path.dirname(path.resolve(process.cwd(), filepath));
+	var dest = {};
+
+	['html', 'js'].forEach(function(type) {
+		var name = basename + '.' + type;
+		dest[type] = {
+			base: base,
+			path: path.join(base, name),
+      name: name
+		};
+	});
+
+	return dest;
+}
+
+module.exports = function (grunt) {
+  grunt.registerMultiTask('crisper', '', function () {
+    var options = this.options({
+      cleanup: true
+    });
+
+    this.files.forEach(function (file) {
+      var contents = file.src.filter(function(filepath) {
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(file) {
+				var content = grunt.file.read(file);
+
+				if (options.cleanup) {
+					grunt.log.warn('Source file "' + file + '" will be removed');
+					grunt.file.delete(file);
+				}
+
+				return content;
+			}).join('\n');
+
+			var destpath = getDestPath(file.dest);
+      var split = crisper.split(contents, destpath.js.name);
+
+      grunt.file.write(destpath.html.path, split.html);
+			grunt.file.write(destpath.js.path, split.js);
+    });
+  });
+};
---
+++ test/test.js
@@ -0,0 +1,84 @@
+/*global describe, before, it*/
+
+'use strict';
+
+var path = require('path');
+var assert = require('assert');
+var grunt = require('grunt');
+var rimraf = require('rimraf');
+var mkdirp = require('mkdirp');
+var opts = grunt.cli.options;
+
+grunt.task.init([]);
+grunt.config.init({});
+grunt.log.muted = false;
+opts.redirect = !opts.silent;
+
+var directory = function directory(dir) {
+  return function directory(done) {
+    process.chdir(__dirname);
+    rimraf(dir, function (err) {
+      if (err) {
+        return done(err);
+      }
+      mkdirp(dir, function (err) {
+        if (err) {
+          return done(err);
+        }
+
+        process.chdir(dir);
+
+        // prepare vulcanized file
+        grunt.file.copy(path.join(__dirname, 'fixtures/vulcanized.html'),
+                        path.join(__dirname, 'tmp/app/vulcanized.html'));
+
+        done();
+      });
+    });
+  };
+};
+
+describe('Crisper grunt task', function () {
+
+  before(directory('tmp'));
+
+  var targets = {
+    dist: {
+      options: {
+				cleanup: false
+			},
+      src: 'app/vulcanized.html',
+      dest: 'dist/vulcanized-csp.html'
+    }
+  };
+
+  it('should generates separated js/html files', function () {
+    grunt.config('crisper', {dist: targets.dist});
+    grunt.task.run('crisper:dist');
+    grunt.task.start();
+
+		var html = grunt.file.read(path.join(__dirname, 'tmp/dist/vulcanized-csp.html'));
+		var js = grunt.file.read(path.join(__dirname, 'tmp/dist/vulcanized-csp.js'));
+		var rex = {
+			js: /Polymer\({/,
+			html: /<script src=\"vulcanized-csp.js\"><\/script><\/body><\/html>/
+		};
+
+    assert(html);
+		assert(js);
+		assert(rex.html.test(html));
+		assert(rex.js.test(js));
+  });
+
+  it('should generates separated js/html files', function () {
+    // turn on cleanup option
+    targets.dist.options.cleanup = true;
+    grunt.config('crisper', {dist: targets.dist});
+    grunt.task.run('crisper:dist');
+    grunt.task.start();
+
+		grunt.file.read(path.join(__dirname, 'tmp/dist/vulcanized-csp.html'));
+
+    assert(!grunt.file.exists(path.join(__dirname, 'tmp/app/vulcanized-csp.html')));
+  });
+});
